23758f1de12729dad8c7d67761921595db55e9c8
Rojan Ulak <ulak.rojan@live.com>
2016-03-07 22:41:12 -0600

    fizzbuzz with build

---
+++ CodingInterview/CodingInterview/Program.cs
@@ -0,0 +1,22 @@
+﻿using FizzBuzzLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzzEngine = new FizzBuzz(int.MaxValue);
+            fizzBuzzEngine.AddTestParm(3, "Fizz");
+            fizzBuzzEngine.AddTestParm(5, "Buzz");
+
+            Console.WriteLine(fizzBuzzEngine.GenerateFizzBuzzResult());
+            Console.Read();
+        }
+    }
+}
